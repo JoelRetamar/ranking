87a6691a9f616f5afb20e3fc16d6ba6e9bfb1f5b
joelretamar <84486915+JoelRetamar@users.noreply.github.com>
2021-05-20 01:31:21 -0300

    Add files via upload

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,7 +3,7 @@ let close = document.querySelectorAll(".close")
 close.forEach(function (close) {
   close.addEventListener("click", function (ev) {
     ev.preventDefault()
-    
+
     let content = document.querySelector(".content")  
     content.classList.remove("animate__animated")  
     content.classList.remove("animate__fadeInDown")  
